adfbe527d33f6dbc7434640577232c3a787a1ffb
zawata <zawataza@gmail.com>
2015-08-16 00:20:59 -0700

    add body movement
    change queue type to integrated
    move input inside body class

--- Snake/Snake/Snake/Body.cs
+++ Snake/Snake/Snake/Body.cs
@@ -5,86 +5,105 @@ using System.Text;
 
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
 
 namespace Snake
 {
     static class Body
     {
-        //static Queue<Body_Segment> body_queue = new Queue<Body_Segment>();
+        static Queue<Body_Segment> body_queue = new Queue<Body_Segment>();
         public static int Max_Segments {get; set;}
         static Body()
         {
-            Max_Segments = 3;
+            Max_Segments = 5;
             addSegment(new Vector2(1,1));
         }
-        public static void move(byte direction)
+        private static int old_direction = 0;
+        private static Vector2 newpos = default(Vector2);
+        public static void move()
         {
             //direction
-            // 3 = up
-            // 4 = down
             // 1 = left
-            // 2 = right
+            // 2 = up
+            // 3 = right
+            // 4 = down
 
-            //Vector2 head_pos = body_queue.Last<Body_Segment>().GridPosition;
-            Vector2 head_pos = LinkedQueue<Body_Segment>.last().GridPosition;
-            Vector2 newpos;
-            switch(direction)
+            Vector2 head_pos = body_queue.Last<Body_Segment>().GridPosition;
+            
+            Keys[] keys = Keyboard.GetState().GetPressedKeys();
+            bool moved = false;
+            if (keys.GetLength(0) > 0)
             {
-                case 1:
-                    newpos = new Vector2(head_pos.X - 1, head_pos.Y);
-                    break;
-                case 2:
-                    newpos = new Vector2(head_pos.X + 1, head_pos.Y);
-                    break;
-                case 3:
-                    newpos = new Vector2(head_pos.X, head_pos.Y - 1);
-                    break;
-                case 4:
-                    newpos = new Vector2(head_pos.X, head_pos.Y+1);
-                    break;
-                default:
-                    throw new Exception("invalid direction: " + direction);
-            }
-            LinearDoubleNode<Body_Segment> temp = LinkedQueue<Body_Segment>.tail;
-            temp = temp.getPrev();
-            Body_Segment temp_body = temp.getElement();
-            if(temp_body.Equals(null))
-            {
-                if (temp_body.GridPosition.Equals(newpos))
+                foreach (Keys key in keys)
                 {
-                    addSegment(newpos);
-                    newpos = default(Vector2);
+                    switch (key)
+                    {
+                        case Keys.Left:
+                                newpos = new Vector2(head_pos.X - 1, head_pos.Y);
+                            moved = true;
+                            old_direction = 1;
+                            break;
+                        case Keys.Up:
+                                newpos = new Vector2(head_pos.X, head_pos.Y - 1);
+                            moved = true;
+                            old_direction = 2;
+                            break;
+                        case Keys.Right:
+                                newpos = new Vector2(head_pos.X + 1, head_pos.Y);
+                            moved = true;
+                            old_direction = 3;
+                            break;
+                        case Keys.Down:
+                                newpos = new Vector2(head_pos.X, head_pos.Y + 1);
+                            moved = true;
+                            old_direction = 4;
+                            break;
+                        default:
+                            break;
+                    }
+                    if (moved)
+                        break;
                 }
-        }
-            drop_last_Segment();
+            }
+            else
+            {
+                switch (old_direction)
+                {
+                    case 1:
+                            newpos = new Vector2(head_pos.X - 1, head_pos.Y);
+                        break;
+                    case 2:
+                            newpos = new Vector2(head_pos.X, head_pos.Y - 1);
+                        break;
+                    case 3:
+                            newpos = new Vector2(head_pos.X + 1, head_pos.Y);
+                        break;
+                    case 4:
+                            newpos = new Vector2(head_pos.X, head_pos.Y + 1);
+                        break;
+                    default:
+                        break;
+                }
+            }
+                addSegment(newpos);
+                drop_last_Segment();
         }
         public static void draw(SpriteBatch spriteBatch)
         {
-            /*foreach (Body_Segment element in body_queue)
+            foreach (Body_Segment element in body_queue)
             {
                 element.Draw(spriteBatch);
-            }
-            */
-            LinearDoubleNode<Body_Segment> temp = LinkedQueue<Body_Segment>.head;
-            temp = LinkedQueue<Body_Segment>.head;
-            for (int i = 0; i < LinkedQueue<Body_Segment>.NodeCount; i++)
-            {
-                temp.getElement().Draw(spriteBatch);
-                temp = temp.getNext();
             }
         }
 
         public static void addSegment(Vector2 gridpos)
         {
-            //body_queue.Enqueue(new Body_Segment(Game1.graphics.GraphicsDevice, gridpos));
-            LinkedQueue<Body_Segment>.enqueue(new Body_Segment(Game1.graphics.GraphicsDevice, gridpos));
+            body_queue.Enqueue(new Body_Segment(Game1.graphics.GraphicsDevice, gridpos));
         }
         public static void drop_last_Segment()
         {
-            //if (Max_Segments >= body_queue.Count) return;
-            if (Max_Segments >= LinkedQueue<Body_Segment>.NodeCount) return;
-            //body_queue.Dequeue();
-            LinkedQueue<Body_Segment>.dequeue();
+            if (Max_Segments >= body_queue.Count) return;
+            body_queue.Dequeue();
         }
         public static int ConvertCoord(int num)
         {
